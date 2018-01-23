b1be2267f6885a909ca28c1c5509e90fdbfc8847
Aart Stuurman <aartstuurman@hotmail.com>
2018-01-23 19:02:46 +0100

    comments

--- src/tcpconnection/includes.h
+++ src/tcpconnection/includes.h
@@ -6,6 +6,7 @@
  *
  *  @documentation private
  */
+// include files from main library
 #include "../includes.h"
 
 // c and c++ dependencies
@@ -24,91 +25,3 @@
 
 // classes that are very commonly used
 #include "addressinfo.h"
-
-/*
-// c and c++ dependencies
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <string>
-#include <memory>
-#include <limits>
-#include <ostream>
-#include <math.h>
-#include <map>
-#include <algorithm>
-#include <unordered_map>
-#include <vector>
-#include <queue>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <netinet/tcp.h>
-#include <functional>
-#include <stdexcept>
-
-// forward declarations
-#include "../include/classes.h"
-
-// utility classes
-#include "../include/endian.h"
-#include "../include/buffer.h"
-#include "../include/bytebuffer.h"
-#include "../include/receivedframe.h"
-#include "../include/outbuffer.h"
-#include "../include/copiedbuffer.h"
-#include "../include/watchable.h"
-#include "../include/monitor.h"
-#include "../include/tcpdefines.h"
-
-// amqp types
-#include "../include/field.h"
-#include "../include/numericfield.h"
-#include "../include/decimalfield.h"
-#include "../include/stringfield.h"
-#include "../include/booleanset.h"
-#include "../include/fieldproxy.h"
-#include "../include/table.h"
-#include "../include/array.h"
-
-// envelope for publishing and consuming
-#include "../include/metadata.h"
-#include "../include/envelope.h"
-#include "../include/message.h"
-
-// mid level includes
-#include "../include/exchangetype.h"
-#include "../include/flags.h"
-#include "../include/callbacks.h"
-#include "../include/deferred.h"
-#include "../include/deferredconsumer.h"
-#include "../include/deferredqueue.h"
-#include "../include/deferreddelete.h"
-#include "../include/deferredcancel.h"
-#include "../include/deferredget.h"
-#include "../include/channelimpl.h"
-#include "../include/channel.h"
-#include "../include/login.h"
-#include "../include/address.h"
-#include "../include/connectionhandler.h"
-#include "../include/connectionimpl.h"
-#include "../include/connection.h"
-#include "../include/tcphandler.h"
-#include "../include/tcpconnection.h"
-
-// classes that are very commonly used
-#include "../include/exception.h"
-#include "../include/protocolexception.h"
-#include "../include/frame.h"
-#include "extframe.h"
-#include "methodframe.h"
-#include "headerframe.h"
-#include "connectionframe.h"
-#include "channelframe.h"
-#include "exchangeframe.h"
-#include "queueframe.h"
-#include "basicframe.h"
-#include "transactionframe.h"
-#include "addressinfo.h"
-*/
