99cc34d922b6a4959ba879602c378ce27a900a88
Aart Stuurman <aartstuurman@hotmail.com>
2018-01-26 22:42:28 +0100

    Added linux_tcp.h so we can now once again use the tcp implementation of the handler.

--- include/amqpcpp.h
+++ include/amqpcpp.h
@@ -29,46 +29,46 @@
 #include <math.h>
 
 // forward declarations
-#include <amqpcpp/classes.h>
+#include "amqpcpp/classes.h"
 
 // utility classes
-#include <amqpcpp/endian.h>
-#include <amqpcpp/buffer.h>
-#include <amqpcpp/bytebuffer.h>
-#include <amqpcpp/receivedframe.h>
-#include <amqpcpp/outbuffer.h>
-#include <amqpcpp/watchable.h>
-#include <amqpcpp/monitor.h>
+#include "amqpcpp/endian.h"
+#include "amqpcpp/buffer.h"
+#include "amqpcpp/bytebuffer.h"
+#include "amqpcpp/receivedframe.h"
+#include "amqpcpp/outbuffer.h"
+#include "amqpcpp/watchable.h"
+#include "amqpcpp/monitor.h"
 
 // amqp types
-#include <amqpcpp/field.h>
-#include <amqpcpp/numericfield.h>
-#include <amqpcpp/decimalfield.h>
-#include <amqpcpp/stringfield.h>
-#include <amqpcpp/booleanset.h>
-#include <amqpcpp/fieldproxy.h>
-#include <amqpcpp/table.h>
-#include <amqpcpp/array.h>
+#include "amqpcpp/field.h"
+#include "amqpcpp/numericfield.h"
+#include "amqpcpp/decimalfield.h"
+#include "amqpcpp/stringfield.h"
+#include "amqpcpp/booleanset.h"
+#include "amqpcpp/fieldproxy.h"
+#include "amqpcpp/table.h"
+#include "amqpcpp/array.h"
 
 // envelope for publishing and consuming
-#include <amqpcpp/metadata.h>
-#include <amqpcpp/envelope.h>
-#include <amqpcpp/message.h>
+#include "amqpcpp/metadata.h"
+#include "amqpcpp/envelope.h"
+#include "amqpcpp/message.h"
 
 // mid level includes
-#include <amqpcpp/exchangetype.h>
-#include <amqpcpp/flags.h>
-#include <amqpcpp/callbacks.h>
-#include <amqpcpp/deferred.h>
-#include <amqpcpp/deferredconsumer.h>
-#include <amqpcpp/deferredqueue.h>
-#include <amqpcpp/deferreddelete.h>
-#include <amqpcpp/deferredcancel.h>
-#include <amqpcpp/deferredget.h>
-#include <amqpcpp/channelimpl.h>
-#include <amqpcpp/channel.h>
-#include <amqpcpp/login.h>
-#include <amqpcpp/address.h>
-#include <amqpcpp/connectionhandler.h>
-#include <amqpcpp/connectionimpl.h>
-#include <amqpcpp/connection.h>
+#include "amqpcpp/exchangetype.h"
+#include "amqpcpp/flags.h"
+#include "amqpcpp/callbacks.h"
+#include "amqpcpp/deferred.h"
+#include "amqpcpp/deferredconsumer.h"
+#include "amqpcpp/deferredqueue.h"
+#include "amqpcpp/deferreddelete.h"
+#include "amqpcpp/deferredcancel.h"
+#include "amqpcpp/deferredget.h"
+#include "amqpcpp/channelimpl.h"
+#include "amqpcpp/channel.h"
+#include "amqpcpp/login.h"
+#include "amqpcpp/address.h"
+#include "amqpcpp/connectionhandler.h"
+#include "amqpcpp/connectionimpl.h"
+#include "amqpcpp/connection.h"
