e3678943cb52c82d4cd1fa9d92f4e84c237b55a0
Raoul Wols <raoulwols@gmail.com>
2021-07-12 14:23:43 +0200

    Monitor earlier in case flush() calls reportError.

--- include/amqpcpp/channelimpl.h
+++ include/amqpcpp/channelimpl.h
@@ -672,18 +672,21 @@ public:
         // skip if there is no oldest callback
         if (!_oldestCallback) return true;
 
+        // we are going to call callbacks that could destruct the channel
+        Monitor monitor(this);
+
         // flush the queue, which will send the next operation if the current operation was synchronous
         flush();
+
+        // the call to flush may have resulted in a call to reportError
+        if (!monitor.valid()) return false;
 
         // copy the callback (so that it will not be destructed during
         // the "reportSuccess" call, if the channel is destructed during the call)
         auto cb = _oldestCallback;
 
         // the call to flush might have caused the callback to have been invoked; check once more
-        if (!cb) return false;
-
-        // we are going to call callbacks that could destruct the channel
-        Monitor monitor(this);
+        if (!cb) return true;
 
         // call the callback
         auto next = cb->reportSuccess(std::forward<Arguments>(parameters)...);
